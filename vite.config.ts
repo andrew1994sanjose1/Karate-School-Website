bf1244e96b85c8a94f5840bcc2b50b775e05728c
andrew1994sanjose1 <andrew1994sanjose@gmail.com>
2026-04-26 05:55:46 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,24 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      'karate-school-website.onrender.com'
+    ]
+  },
+  preview: {
+    allowedHosts: [
+      'karate-school-website.onrender.com'
+    ]
+  },
   resolve: {
     alias: {
       'motion/react': 'framer-motion',
       'motion': 'framer-motion',
-      'qrcode.react': 'qrcode.react' // Siguraduhin na mahanap ang QR code
+      'qrcode.react': 'qrcode.react'
     },
   },
   build: {
-    rollupOptions: {
-      external: [], // Huwag i-externalize ang qrcode
-    },
     commonjsOptions: {
       include: [/qrcode.react/, /node_modules/],
     },
